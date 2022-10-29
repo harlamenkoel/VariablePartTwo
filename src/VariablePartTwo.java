4efc6fe06c69bd84484d3cf5f31622b86703ab72
harlamenkoel <harlamenkoel@gmail.com>
2022-10-29 23:04:06 +0700

    Task 1

--- src/VariablePartTwo.java
+++ src/VariablePartTwo.java
@@ -1,5 +1,17 @@
 public class VariablePartTwo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        byte  wholeNumber1 = 101;
+        short wholeNumber2 = 1001;
+        int wholeNumber3 = 10001;
+        long wholeNumber4 = 1_000_001L;
+        float  fractionalNumber1 = 1.5F;
+        double  fractionalNumber2 = 10.101;
+        System.out.println("Значение переменной wholeNumber1 с типом byte равно "+wholeNumber1);
+        System.out.println("Значение переменной wholeNumber2 с типом short равно "+wholeNumber2);
+        System.out.println("Значение переменной wholeNumber3 с типом int равно "+wholeNumber3);
+        System.out.println("Значение переменной wholeNumber4 с типом long равно "+wholeNumber4);
+        System.out.println("Значение переменной fractionalNumber1  с типом byte равно "+fractionalNumber1);
+        System.out.println("Значение переменной fractionalNumber2 с типом byte равно "+fractionalNumber2);
     }
 }
